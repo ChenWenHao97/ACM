2745755aae097ea6db9ac207cf828242e8d7ee1b
kevin19970820 <www.572043666mo@qq.com>
2018-05-09 16:56:26 +0800

    work

--- greedy/delete_num.cpp
+++ greedy/delete_num.cpp
@@ -1,3 +1,19 @@
+/*
+删数问题
+Time Limit: 1000MS Memory Limit: 65536KB
+Submit Statistic
+Problem Description
+ 键盘输入一个高精度的正整数n（≤100位），去掉其中任意s个数字后剩下的数字按照原来的左右次序组成一个新的正整数。
+ 编程对给定的n与s，寻找一种方案，使得剩下的数字组成的新数最小。
+Input
+  输入有多组 每组包括原始数n，要去掉的数字数s；
+Output
+ 输出去掉s个数后最小的数
+Example Input
+178543  4
+Example Output
+13
+*/
 #include<iostream>
  #include<string>
  using namespace std;
